80323e733cc60b6ccc2baa4453119d2c7ba43565
lucianogarzo <lucianogarzo@gmail.com>
2026-01-10 23:45:26 -0300

    Delete functions directory

--- functions/callback.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  // Send back to CMS; Decap will complete auth in-browser (PKCE)
-  return Response.redirect(`${url.origin}/admin/`, 302);
-}
