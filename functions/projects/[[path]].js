138cdc7e275dcea2bcc76d741d5bf7d76784556e
lucianogarzo <lucianogarzo@gmail.com>
2026-01-10 23:22:25 -0300

    Update [[path]].js

--- functions/projects/[[path]].js
+++ functions/projects/[[path]].js
@@ -1,23 +1,14 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
 
-  // allow /projects and /projects/ to work normally (grid page)
+  // Dejar pasar el listado
   if (url.pathname === "/projects" || url.pathname === "/projects/") {
     return context.next();
   }
 
-  // rewrite any /projects/<anything> to /projects/project.html
-  const templateUrl = new URL("/projects/project.html", url.origin);
+  // Reescribir a la plantilla (SIN fetch)
+  url.pathname = "/projects/project.html";
 
-  const res = await fetch(templateUrl.toString(), {
-    headers: context.request.headers,
-  });
-
-  return new Response(res.body, {
-    status: 200,
-    headers: {
-      "content-type": "text/html; charset=UTF-8",
-      "cache-control": "no-store",
-    },
-  });
+  const newRequest = new Request(url.toString(), context.request);
+  return context.env.ASSETS.fetch(newRequest);
 }
